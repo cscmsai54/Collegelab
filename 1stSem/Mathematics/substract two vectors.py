cd10582e191b10e860885422cde3b6548bb54058
unknown <ajayprasad0008@gmail.com>
2022-11-07 15:00:44 +0530

    Substract Two Numbers

---
+++ 1stSem/Mathematics/substract two vectors.py
@@ -0,0 +1,15 @@
+import numpy as np
+vectorlist1 = []
+vectorlist2 = []
+print("Enter Vector 1 : ")
+for j in range(3):
+    j = int(input(">>"))
+    vectorlist1.append(j)
+print("Enter Vector 2 : ")
+for j in range(3):
+    j = int(input(">>"))
+    vectorlist2.append(j)
+vector1 = np.array(vectorlist1)
+vector2 = np.array(vectorlist2)
+subbed = vector1 - vector2
+print(vector1, " - ", vector2, " = ", subbed)
