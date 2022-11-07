f773a9fb292415bc8dbc09182c2bc496a9165041
unknown <ajayprasad0008@gmail.com>
2022-11-07 14:50:39 +0530

    add two vectors completed

--- 1stSem/Mathematics/add two vectors.py
+++ 1stSem/Mathematics/add two vectors.py
@@ -1,1 +1,21 @@
 import numpy as np
+
+vectorlist1 = []
+vectorlist2 = []
+
+print("Enter Elements of Vector 1 : ")
+for i in range(3):
+    j = int(input(">>"))
+    vectorlist1.append(j)
+    
+print("Enter Elements of Vector 2 : ")
+for i in range(3):
+    j = int(input(">>"))
+    vectorlist2.append(j)
+
+vector1 = np.array(vectorlist1)
+vector2 = np.array(vectorlist2)
+
+added = vector1 + vector2 
+
+print(vector1 , " + " , vector2 , " = " , added)
