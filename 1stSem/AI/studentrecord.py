6d3e9dc38b6be6db99b6433407c9514b6039bd4f
unknown <ajayprasad0008@gmail.com>
2022-11-10 15:43:16 +0530

    Student Record Completed

--- 1stSem/AI/studentrecord.py
+++ 1stSem/AI/studentrecord.py
@@ -3,13 +3,27 @@ data = {
     "RollNumber" : [121,122,123,124],
     "Name" : ["student1","student2", "student3", "student4" ],
     "subject-1" : [80,70,76,54],
-    "subject-2" : [99,69,55,88],
+    "subject-2" : [69,55,88,99],
     "subject-3" : [66,77,55,67],
     "subject-4" : [78,90,100,78],
     "subject-5" : [56,99,67,45]
 }
 df = pd.DataFrame( data )
-#print ("DataFrame : \n " ,df)  
+print ("DataFrame : \n " ,df)  
 p = df.idxmax(numeric_only=True)
-#print(p[1])
-print(df.index)
+g = df.max()
+subs = list(df.columns)
+for i in range (1,6):
+    sub= subs[i+1]
+    sname = df.at[p[i], 'Name']
+    rn = df.at[p[i], 'RollNumber']
+    score = g[i+1]
+    print(f"Subject : {sub}    Student Name : {sname}     RollNumber : {rn}     Score : {score}")
+
+
+
+
+
+
+
+
