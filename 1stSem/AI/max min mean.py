c62b181c89379e08e4f1b0c2d73d5f471060f949
unknown <ajayprasad0008@gmail.com>
2022-11-14 14:30:02 +0530

    Minimum maximum mean

---
+++ 1stSem/AI/max min mean.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv("numeric dataset.csv")
+
+print(df)
+
+print("\n>>>Maximum<<<\n" ,df.max())
+print("\n>>>Minimum<<<\n" ,df.min())
+print("\n>>>Mean<<<\n" ,df.mean())
--- 1stSem/AI/standard deviation.py
+++ 1stSem/AI/standard deviation.py
@@ -1,4 +1,4 @@
 import pandas as pd
 df = pd.read_csv("numeric dataset.csv")
-print(df.to_string())
+print(df.to_string(), "\n")
 print(df.std())
