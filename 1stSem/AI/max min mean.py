2f4ba2dfe6452ab69a27ce5dd5487e358974fb6e
unknown <ajayprasad0008@gmail.com>
2022-11-25 12:33:27 +0530

    norm commit 😎

--- 1stSem/AI/max min mean.py
+++ 1stSem/AI/max min mean.py
@@ -1,6 +1,23 @@
 import pandas as pd
+def minimum():
+    
+    
+    return min
+
+def maximum():
+    
+    
+    return max 
+
+def mean():
+    
+    
+    
+    return mean
+    
 df = pd.read_csv("numeric dataset.csv")
 print(df)
-print("\n>>>Minimum<<<\n" ,df.min())
-print("\n>>>Maximum<<<\n" ,df.max())
-print("\n>>>Mean<<<\n" ,df.mean())
+for column in df.columns:
+    print("\n>>>Minimum<<<\n" ,minimum(df[column]))
+    print("\n>>>Maximum<<<\n" ,maximum(df[column]))
+    print("\n>>>Mean<<<\n" ,mean(df[column]))
