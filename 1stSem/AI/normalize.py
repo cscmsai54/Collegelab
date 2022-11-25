ff7ea6d429f5e40b47ae617391ea70afa18363ac
unknown <ajayprasad0008@gmail.com>
2022-11-25 12:26:12 +0530

    Normalize Without Function 😎

--- 1stSem/AI/normalize.py
+++ 1stSem/AI/normalize.py
@@ -1,10 +1,22 @@
 import pandas as pd
-from sklearn import preprocessing
+def normalize(elements):
+    min = elements.min()
+    max = elements.max()
+    low = max - min 
+    col = []
+    for element in elements:
+        upper = element - min
+        norm = upper / low
+        norm = round(norm ,3)
+        col.append(norm)
+    return col
 df = pd.read_csv("numeric dataset.csv")
-print(df)
+ndf = pd.DataFrame()
+print("\nDataFrame :\n\n ", df)
+for column in df.columns:
+    normalized_df= normalize(df[column])
+    ndf[column]=normalized_df
+print("\n Normalized Data : \n\n" ,ndf)
 
-#normalized_df = ((df - df.mean()) / df.std)
-normalized_df= preprocessing.MinMaxScaler()
 
-print(normalized_df)
 
