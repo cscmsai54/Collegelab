5936dfab39620760e6a02d3768cfc9817d8ec34b
unknown <ajayprasad0008@gmail.com>
2022-11-28 14:21:59 +0530

    Completed Min Max Mean 😋

--- 1stSem/AI/max min mean.py
+++ 1stSem/AI/max min mean.py
@@ -1,23 +1,30 @@
 import pandas as pd
-def minimum():
-    
+def minimum(elements):
+    min = 99999
+    for element in elements:
+        if element < min:
+            min = element
     
     return min
 
-def maximum():
-    
+def maximum(elements):
+    max = -99999
+    for element in elements:
+        if element > max :
+            max = element
     
     return max 
 
-def mean():
-    
-    
-    
+def mean(elements):
+    sum = 0
+    for element in elements:
+        sum = sum + element
+    length = len(elements)
+    mean = sum/length
     return mean
     
 df = pd.read_csv("numeric dataset.csv")
 print(df)
 for column in df.columns:
-    print("\n>>>Minimum<<<\n" ,minimum(df[column]))
-    print("\n>>>Maximum<<<\n" ,maximum(df[column]))
-    print("\n>>>Mean<<<\n" ,mean(df[column]))
+    print (f'<<< In {column} >>>')
+    print("Minimum >>" ,minimum(df[column]), "Maximum >>" ,maximum(df[column]), "Mean >>" ,mean(df[column]))
