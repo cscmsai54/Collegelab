ac8c5b4d557a728b5831211e1e1138faa83ed05d
unknown <ajayprasad0008@gmail.com>
2022-12-15 12:19:51 +0530

    Added Initiation in TSP

--- 1stSem/ACO/tsp.py
+++ 1stSem/ACO/tsp.py
@@ -1,16 +1,37 @@
+def pathlength(n, adj):
+    print("hi")
+    visited = []
+    non_visited = list(range(0,n))
+    print(non_visited)
+    
 
 
+p = []
+adj_matrix = []
 
 
+print("\n\nEnter The Number Of Nodes/Cities :")
+nodes = int(input(">>"))
 
+print("Enter Distance frome each Nodes : ")
+for i in range (nodes):
+    row = []
+    for j in range(nodes):
+        if i == j :
+            v = 0
+            row.append(v)
+        else :
+            print(f"Enter Distance Frome Nodes {i+1} to {j+1}")
+            v = int(input(">>"))
+            row.append(v)
+    adj_matrix.append(row)
+        
+for i in range(0,nodes):
+    print("\n")
+    print(adj_matrix[i])
 
-
-print("Enter The Number Of Nodes :")
-
-
-
-
-
+    val = pathlength( i , adj_matrix)
+    p.append(val)
 
 
 
