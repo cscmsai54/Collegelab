f468ddc96c509028e80916e64c3b744a4f9b0864
unknown <ajayprasad0008@gmail.com>
2023-01-11 15:30:53 +0530

    completed Armstrong

--- uncategorised/Armstrong.py
+++ uncategorised/Armstrong.py
@@ -1,9 +1,11 @@
+import math
+
 def countDigit(number):
     count = 0
     while number :
         count = count +1
-        number = number/10
-    
+        number = number//10
+    return count
 
 num = int(input("Enter the Number: \n>>"))
 power = countDigit(num)
@@ -12,7 +14,11 @@ sum = 0
 
 while check :    
     mod = check % 10
-    check = check/10
-    add = pow(mod, power)
+    check = check//10
+    add = math.pow(mod, power)
     sum = sum + add
-    
+
+if num == sum :
+    print(f"\n Your number {num} Is Armstrong.....")
+else :
+    print(f"\n Your number {num} Is NOT AN Armstrong.....")    
